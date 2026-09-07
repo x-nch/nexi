a29fbc625e797d1b1b2a986b61b950a68b0f0ffb
xnch <charlie.xnch@gmail.com>
2026-09-08 02:05:48 +0530

    fix(config): remove env_file to rely on environment variables only

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_prefix="NEXI_", env_file=".env")
+    model_config = SettingsConfigDict(env_prefix="NEXI_")
 
     # Prometheus instrumentation
     metrics_enabled: bool = True
